d088c00dca102f84e28735990901e76536bc2324
Aditya Anand <adi11aug@gmail.com>
2026-06-01 18:02:39 +0530

    Added history test page

---
+++ app/tests/history-01/page.tsx
@@ -0,0 +1,37 @@
+export default function HistoryTestPage() {
+  return (
+    <main className="p-10">
+      <h1 className="text-4xl font-bold">
+        History Test 01
+      </h1>
+
+      <p className="mt-4">
+        Question 1
+      </p>
+
+      <p>
+        Who designed the city of Chandigarh?
+      </p>
+
+      <div className="mt-4 space-y-2">
+
+        <button className="block border p-2 rounded">
+          A. Le Corbusier
+        </button>
+
+        <button className="block border p-2 rounded">
+          B. Frank Lloyd Wright
+        </button>
+
+        <button className="block border p-2 rounded">
+          C. Louis Kahn
+        </button>
+
+        <button className="block border p-2 rounded">
+          D. Walter Gropius
+        </button>
+
+      </div>
+    </main>
+  );
+}
